d11838e8e4a1ad04552f2cac0544552972c55763
cchavezmx <cchavezmx@outlook.com>
2022-10-14 01:51:19 -0500

    puerto

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 const graphqlSchema = require('./graphql/schema')
 const graphqlResolvers = require('./graphql/resolvers')
 
+const PORT = process.env.PORT
+
 const app = express()
 
 app.use('/graphql', graphqlHttp({
@@ -18,5 +20,5 @@ const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWOR
 const options = {useNewUrlParser: true, useUnifiedTopology: true}
 
 mongoose.connect(uri, options)
-        .then(() => app.listen(3000, console.log('🚀🚀🚀🚀 Server running on port 3000')))
+        .then(() => app.listen(PORT, console.log('🚀🚀🚀🚀 Server running on port 3000')))
         .catch(error => { throw error })
